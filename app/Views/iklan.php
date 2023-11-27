63d4faac7a8e60eb444c29da5176cf1f994c2433
mashitaad <mashitaad@gmail.com>
2023-11-27 23:53:46 +0700

    feat: home

---
+++ app/Views/iklan.php
@@ -0,0 +1,7 @@
+<header class="py-0">
+    <div class="">
+        <div class="text-center text-white">
+            <img src="/images/poster.jpeg" alt="Shop in style" class="img-fluid">
+        </div>
+    </div>
+</header>
